ea7e258e0c249ce8b3d89383308fb6a8daa40e52
Felipe Teixeira de Lima <felipe.teixeiralima2511@gmail.com>
2026-03-02 09:16:37 -0300

    feito até o exercício b)

---
+++ exercicios/2026-03-02/a.js
@@ -0,0 +1,7 @@
+// Testando typeof
+
+console.log(typeof "teste"); // string
+console.log(typeof 6.7); // number
+console.log(typeof false); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
